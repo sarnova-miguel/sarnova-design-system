80e1791eaad860f79f582a8d8e10c8562aa89274
Miguel Ramos <Miguel.Ramos@sarnova.com>
2025-12-04 10:09:27 -0500

    All badge added

--- components/elements/Colors.tsx
+++ components/elements/Colors.tsx
@@ -7,6 +7,7 @@ import BTBadge from "../BTBadge";
 import TABadge from "../TABadge";
 import CPBadge from "../CPBadge";
 import DTBadge from "../DTBadge";
+import ABBadge from "../ABBadge";
 
 const Colors = () => {
   const { brand } = useBrand();
@@ -15,6 +16,15 @@ const Colors = () => {
     <div className="mb-10">
       <SectionSubHeader anchor="colors">Colors</SectionSubHeader>
       <SectionDesc>Primary and secondary colors per brand</SectionDesc>
+      {(brand === "all" ||
+        brand === "bt" ||
+        brand === "ta" ||
+        brand === "cp" ||
+        brand === "dt") && (
+        <div className="mb-6">
+          <ABBadge/>
+        </div>
+      )}
       {(brand === "all" || brand === "bt") && (
         <div className="mb-6">
           <BTBadge />
